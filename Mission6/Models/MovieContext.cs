368901bbf5f8f0eea8a3d20d4bcd8cb501637abe
Jared Gastelum <jaredgastelum@gmail.com>
2023-02-21 08:17:26 -0800

    everything is fixed

--- Mission6/Models/MovieContext.cs
+++ Mission6/Models/MovieContext.cs
@@ -22,41 +22,41 @@ namespace Mission6.Models
 
                 new ApplicationResponse
                 {
-                    MovieID: 1,
-                    Category: "Action/Adventure",
-                    Title: "Harry Potter and the Deathly Hallows – Part 2",
-                    Year: "2011",
-                    Director: "David Yates",
-                    Rating: "PG-13",
-                    Edited: "",
-                    LentTo: "",
-                    Notes: "",
+                    MovieID = 1,
+                    Category = "Action/Adventure",
+                    Title = "Harry Potter and the Deathly Hallows – Part 2",
+                    Year = 2011,
+                    Director = "David Yates",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
                 },
 
                new ApplicationResponse
                 {
-                    MovieID: 2,
-                    Category: "Comedy",
-                    Title: "Pink Panther",
-                    Year: "2006",
-                    Director: "Shawn Levy",
-                    Rating: "PG",
-                    Edited: "",
-                    LentTo: "",
-                    Notes: "",
+                    MovieID = 2,
+                    Category = "Comedy",
+                    Title = "Pink Panther",
+                    Year = 2006,
+                    Director = "Shawn Levy",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
                 },
 
               new ApplicationResponse
                 {
-                    MovieID: 3,
-                    Category: "Action/Adventure",
-                    Title: "Inception",
-                    Year: "2010",
-                    Director: "Christopher Nolan",
-                    Rating: "PG-13",
-                    Edited: "",
-                    LentTo: "",
-                    Notes: "",
+                    MovieID = 3,
+                    Category = "Action/Adventure",
+                    Title = "Inception",
+                    Year = 2010,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
                 }
 
                 );
